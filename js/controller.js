b3b18b07e2ecb62d19fcd757ac149f7b9296a163
Marcos Pereira <marcospereira.gnr@gmail.com>
2018-01-07 00:21:06 -0300

    modal with player statistcs

---
+++ js/controller.js
@@ -0,0 +1,22 @@
+
+
+function getMarcos (){
+    return "Marcos Lourinho";
+}
+
+function getRobson(){
+    return "Robson Andrade";
+}
+
+function getJairo(){
+    return "Jairo Corrêa";
+}
+
+function getDalton(){
+    return "Dalton Franco";
+}
+
+function getDalton(){
+    return "Lucas Rodrigues";
+}
+
